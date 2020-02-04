c4439a934d898d538c6fc84911587f17a5e98236
brenersilva <rib.silva@outlook.com>
2020-02-04 17:57:54 -0300

    Setting Nodemailer.

---
+++ backend/src/config/mail.js
@@ -0,0 +1,12 @@
+export default {
+  host: 'smtp.mailtrap.io',
+  port: 2525,
+  secure: false,
+  auth: {
+    user: 'dffdf24c2343a9',
+    pass: 'ce3919c4543f8d',
+  },
+  default: {
+    from: 'Equipe GoBarber <noreply@gobarber.com>',
+  },
+};
